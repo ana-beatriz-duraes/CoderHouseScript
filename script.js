a651b67f5ce47669ecdd498a4095992d23fbed1d
Ana Beatriz <anabeatrizduraes326@gmail.com>
2023-10-18 21:22:12 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var n1 = prompt("Insira o primerior numero :");
+var n2 = prompt("Insira o segundo numero: ");
+
+var resultado = parseInt(n1) + parseInt(n2);
+console.log(resultado);
+alert(resultado);
+
+var resultadoMult = parseInt (n1) * parseInt(n2);
+console.log(resultadoMult);
+alert(resultadoMult);git 
